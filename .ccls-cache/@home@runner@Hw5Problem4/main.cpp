ee98e065b3cd580ac083d36776830ce91d1a59fd
apmcguan <111770168+apmcguan@users.noreply.github.com>
2022-09-26 04:59:07 +0000

    cin a-j

--- .ccls-cache/@home@runner@Hw5Problem4/main.cpp
+++ .ccls-cache/@home@runner@Hw5Problem4/main.cpp
@@ -8,5 +8,30 @@ int main() {
   int a;
   int m = 42;
   int count = 1;
+
+  cin >> a;
+    int moda = a % m;
+  cin >> a;
+    int modb = a % m;
+  cin >> a;
+    int modc = a % m;
+  cin >> a;
+    int modd = a % m;
+  cin >> a;
+    int mode = a % m;
+  cin >> a;
+    int modf = a % m;
+  cin >> a;
+    int modg = a % m;
+  cin >> a;
+    int modh = a % m;
+  cin >> a;
+    int modi = a % m;
+  cin >> a;
+    int modj = a % m;
+  
+  
+  
+  
   
 }
