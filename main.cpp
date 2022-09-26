422db35e2b45910d4523b4d612b9a15cbe99ca7e
apmcguan <111770168+apmcguan@users.noreply.github.com>
2022-09-26 05:17:31 +0000

    if

--- main.cpp
+++ main.cpp
@@ -29,6 +29,45 @@ int main() {
     int modi = a % m;
   cin >> a;
     int modj = a % m;
+  if (modb!= moda)
+  {
+    count ++;
+  }
+  if ((modc != moda) && (modc != modb))
+  {
+    count ++;
+  }
+  if ((modd != moda) && (modd != modb) && (modd != modc))
+  {
+    count ++;
+    }
+  if ((mode != moda) && (mode != modb) && (mode != modc) && (mode != modd))
+  {
+    count ++;
+  }
+  if ((modf != moda) && (modf != modb) && (modf != modc) && (modf != modd)&&(modf != mode))
+  {
+    count ++;
+  }
+  if ((modg != moda) && (modg != modb) && (modg != modc) && (modg != modd)&&(modg != mode) && (modg != modf))
+  {
+    count ++;
+  }
+  if ((modh != moda) && (modh != modb) && (modh != modc) && (modh != modd)&&(modh != mode) && (modh != modf) &&(modh != modg))
+  {
+    count ++;
+  }
+   if ((modi != moda) && (modi!= modb) && (modi != modc) && (modi != modd)&&(modi != mode) && (modi != modf) &&(modi != modg) &&(modi != modh))
+  {
+    count ++;
+  }
+  if ((modj != moda) && (modj != modb) && (modj != modc) && (modj != modd)&&(modj != mode) && (modj != modf) &&(modj != modg) &&(modj != modh)&& (modj != modh))
+  {
+    count ++;
+  }
+  cout << count <<endl;
+  
+  
   
   
   
